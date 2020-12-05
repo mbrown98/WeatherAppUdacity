c16a532855c5142c5f4c38e5c918d6abeecea44e
Matt Brown <matthewbrown0798@gmail.com>
2020-12-04 20:08:22 -0500

    completed basic server setup

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
-
+const port = 8000;
 // Setup Server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
